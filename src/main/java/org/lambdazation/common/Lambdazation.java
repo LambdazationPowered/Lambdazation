6747e7464f074b49201ab3cd82193dbe286597a3
yuxuanchiadm <yuxuanchiadm@126.com>
2019-02-26 13:29:36 +0800

    Add test item

--- src/main/java/org/lambdazation/common/Lambdazation.java
+++ src/main/java/org/lambdazation/common/Lambdazation.java
@@ -2,33 +2,67 @@ package org.lambdazation.common;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lambdazation.common.item.ItemLambdaCrystal;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod("lambdazation")
 public final class Lambdazation {
-	private final Logger logger;
+	public final Logger logger;
+	public final Items items;
+	public final Blocks blocks;
+	public final EventHandlers eventHandlers;
 
 	public Lambdazation() {
 		logger = LogManager.getLogger();
+		items = new Items();
+		blocks = new Blocks();
+		eventHandlers = new EventHandlers();
 
-		MinecraftForge.EVENT_BUS.register(this);
-		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+		MinecraftForge.EVENT_BUS.register(eventHandlers);
+		FMLJavaModLoadingContext.get().getModEventBus().register(eventHandlers);
 	}
 
-	@SubscribeEvent
-	public void registerBlocks(RegistryEvent.Register<Block> e) {
-		logger.info("registerBlocks");
+	public final class Items {
+		public final ItemLambdaCrystal itemLambdaCrystal;
+
+		public Items() {
+			itemLambdaCrystal = new ItemLambdaCrystal(new Item.Properties());
+			itemLambdaCrystal.setRegistryName(new ResourceLocation("lambdazation:lambda_crystal"));
+		}
+
+		public void registerItems(IForgeRegistry<Item> registry) {
+			registry.register(itemLambdaCrystal);
+		}
 	}
 
-	@SubscribeEvent
-	public void registerItems(RegistryEvent.Register<Item> e) {
-		logger.info("registerItems");
+	public final class Blocks {
+		public Blocks() {
+
+		}
+
+		public void registerBlocks(IForgeRegistry<Block> registry) {
+
+		}
+	}
+
+	public final class EventHandlers {
+		@SubscribeEvent
+		public void registerItems(RegistryEvent.Register<Item> e) {
+			items.registerItems(e.getRegistry());
+		}
+
+		@SubscribeEvent
+		public void registerBlocks(RegistryEvent.Register<Block> e) {
+			blocks.registerBlocks(e.getRegistry());
+		}
 	}
 }
---
+++ src/main/java/org/lambdazation/common/item/ItemLambdaCrystal.java
@@ -0,0 +1,9 @@
+package org.lambdazation.common.item;
+
+import net.minecraft.item.Item;
+
+public final class ItemLambdaCrystal extends Item {
+	public ItemLambdaCrystal(Properties properties) {
+		super(properties);
+	}
+}
