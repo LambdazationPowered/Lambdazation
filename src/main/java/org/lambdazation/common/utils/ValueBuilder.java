cbace2062cdb133a2c0639bf39ed7563dd4f21e6
yuxuanchiadm <yuxuanchiadm@126.com>
2019-03-12 23:30:37 +0800

    Workaround type inference bug in javac

--- src/main/java/org/lambdazation/common/utils/ValueBuilder.java
+++ src/main/java/org/lambdazation/common/utils/ValueBuilder.java
@@ -6,36 +6,41 @@ import java.util.function.Supplier;
 
 public final class ValueBuilder {
 	public static <T> T build(T value, Consumer<? super T> builder) {
-		return define(() -> value, builder, Function.identity()).get();
+		return internal(() -> value, builder, Function.identity()).get();
 	}
 
 	public static <T> T build(T value, Consumer<? super T> builder, Function<? super T, ? extends T> finalizer) {
-		return define(() -> value, builder, Function.identity()).get();
+		return internal(() -> value, builder, Function.identity()).get();
 	}
 
 	public static <T> T build(Supplier<? extends T> constructor, Consumer<? super T> builder) {
-		return define(constructor, builder, Function.identity()).get();
+		return internal(constructor, builder, Function.identity()).get();
 	}
 
 	public static <T> T build(Supplier<? extends T> constructor, Consumer<? super T> builder,
 		Function<? super T, ? extends T> finalizer) {
-		return define(constructor, builder, finalizer).get();
+		return internal(constructor, builder, finalizer).get();
 	}
 
 	public static <T> Supplier<? extends T> define(T value, Consumer<? super T> builder) {
-		return define(() -> value, builder, Function.identity());
+		return internal(() -> value, builder, Function.identity());
 	}
 
 	public static <T> Supplier<? extends T> define(T value, Consumer<? super T> builder,
 		Function<? super T, ? extends T> finalizer) {
-		return define(() -> value, builder, Function.identity());
+		return internal(() -> value, builder, Function.identity());
 	}
 
 	public static <T> Supplier<? extends T> define(Supplier<? extends T> constructor, Consumer<? super T> builder) {
-		return define(constructor, builder, Function.identity());
+		return internal(constructor, builder, Function.identity());
 	}
 
 	public static <T> Supplier<? extends T> define(Supplier<? extends T> constructor, Consumer<? super T> builder,
+		Function<? super T, ? extends T> finalizer) {
+		return internal(constructor, builder, finalizer);
+	}
+
+	private static <T> Supplier<? extends T> internal(Supplier<? extends T> constructor, Consumer<? super T> builder,
 		Function<? super T, ? extends T> finalizer) {
 		return () -> {
 			T value = constructor.get();
