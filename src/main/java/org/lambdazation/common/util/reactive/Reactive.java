ac7b874da93cf3e567b5c0a48a7e33fa26d6396a
yuxuanchiadm <yuxuanchiadm@126.com>
2019-04-16 02:30:19 +0800

    Add port

--- src/main/java/org/lambdazation/common/util/reactive/Reactive.java
+++ src/main/java/org/lambdazation/common/util/reactive/Reactive.java
@@ -1,26 +1,67 @@
 package org.lambdazation.common.util.reactive;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 import org.lambdazation.common.util.data.Unit;
 
 public final class Reactive {
+	private boolean responsive;
+	private boolean processing;
+	private final List<Port<?>> ports;
+
+	Reactive(boolean responsive, boolean processing, List<Port<?>> ports) {
+		this.responsive = responsive;
+		this.processing = processing;
+		this.ports = ports;
+	}
+
 	public void resume() {
-		// TODO NYI
-		throw new AbstractMethodError();
+		if (responsive)
+			return;
+		responsive = true;
+		ports.forEach(Port::attach);
 	}
 
 	public void suspend() {
-		// TODO NYI
-		throw new AbstractMethodError();
+		if (!responsive)
+			return;
+		responsive = true;
+		ports.forEach(Port::detach);
 	}
 
 	public State state() {
-		// TODO NYI
-		throw new AbstractMethodError();
+		return processing ? State.PROCESSING : responsive ? State.RESUMED : State.SUSPENDED;
 	}
 
 	public static Reactive build(Flow<Unit> flow) {
 		// TODO NYI
 		throw new AbstractMethodError();
+	}
+
+	static final class Port<A> {
+		final Source<A> source;
+		final Consumer<A> callback;
+		Source.Handler<A> handler;
+
+		Port(Source<A> source, Consumer<A> callback) {
+			this.source = source;
+			this.callback = callback;
+			this.handler = null;
+		}
+
+		void attach() {
+			if (handler != null)
+				throw new IllegalStateException();
+			handler = source.attach(callback);
+		}
+
+		void detach() {
+			if (handler == null)
+				throw new IllegalStateException();
+			handler.detach();
+			handler = null;
+		}
 	}
 
 	public enum State {
--- src/main/java/org/lambdazation/common/util/reactive/Source.java
+++ src/main/java/org/lambdazation/common/util/reactive/Source.java
@@ -3,7 +3,7 @@ package org.lambdazation.common.util.reactive;
 import java.util.function.Consumer;
 
 public interface Source<A> {
-	Handler<A> attach(Consumer<A> handler);
+	Handler<A> attach(Consumer<A> callback);
 
 	interface Handler<A> {
 		void detach();
