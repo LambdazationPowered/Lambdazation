c91537155b7241b46a9ffa8739d91d8ff8a4fc51
yuxuanchiadm <yuxuanchiadm@126.com>
2019-05-13 19:55:35 +0800

    Update

--- src/main/java/org/lambdazation/client/gui/widget/WidgetBase.java
+++ src/main/java/org/lambdazation/client/gui/widget/WidgetBase.java
@@ -1,5 +1,6 @@
 package org.lambdazation.client.gui.widget;
 
+import it.unimi.dsi.fastutil.ints.IntSet;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -11,27 +12,61 @@ public class WidgetBase<M extends ModelBase> {
 		this.model = model;
 	}
 
-	public void draw() {
+	public void draw(DrawContext ctx) {
 
 	}
 
-	public void onKeyboardChar(int modifiers, char input) {
+	public void onKeyboardKey(KeyboardContext ctx, int key, boolean pressed) {
 
 	}
 
-	public void onKeyboardKey(int modifiers, int key, int scancode, boolean pressed) {
+	public void onKeyboardChar(KeyboardContext ctx, char input) {
 
 	}
 
-	public void onMouseButton(double localX, double localY, int button, boolean pressed) {
+	public void onMouseButton(MouseContext ctx, int button, boolean pressed) {
 
 	}
 
-	public void onMouseMove(double localX, double localY, double deltaX, double deltaY) {
+	public void onMouseMove(MouseContext ctx, double deltaX, double deltaY) {
 
 	}
 
-	public void onMouseWheel(double localX, double localY, double delta) {
+	public void onMouseWheel(MouseContext ctx, double delta) {
 
+	}
+
+	public static final class DrawContext {
+		public final double partialTicks;
+		public final KeyboardContext keyboardContext;
+		public final MouseContext mouseContext;
+
+		public DrawContext(double partialTicks, KeyboardContext keyboardContext, MouseContext mouseContext) {
+			this.partialTicks = partialTicks;
+			this.keyboardContext = keyboardContext;
+			this.mouseContext = mouseContext;
+		}
+	}
+
+	public static final class KeyboardContext {
+		public final int modifiers;
+		public final IntSet keyPressed;
+
+		public KeyboardContext(int modifiers, IntSet keyPressed) {
+			this.modifiers = modifiers;
+			this.keyPressed = keyPressed;
+		}
+	}
+
+	public static final class MouseContext {
+		public final double localX;
+		public final double localY;
+		public final IntSet buttonPressed;
+
+		public MouseContext(double localX, double localY, IntSet buttonPressed) {
+			this.localX = localX;
+			this.localY = localY;
+			this.buttonPressed = buttonPressed;
+		}
 	}
 }
