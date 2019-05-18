ed2304cb7d8dec1c9b09d5cc82496a9d76793b79
yuxuanchiadm <yuxuanchiadm@126.com>
2019-05-18 13:48:15 +0800

    Update

--- src/main/java/org/lambdazation/client/gui/widget/view/ViewButton.java
+++ src/main/java/org/lambdazation/client/gui/widget/view/ViewButton.java
@@ -1,6 +1,7 @@
 package org.lambdazation.client.gui.widget.view;
 
 import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
@@ -66,13 +67,62 @@ public class ViewButton<M extends ModelBase> extends ViewBase<M> {
 		}
 
 		ctx.minecraft.getTextureManager().bindTexture(resource);
+
+		double textureWidth = GlStateManager.glGetTexLevelParameteri(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_WIDTH);
+		double textureHeight = GlStateManager.glGetTexLevelParameteri(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_HEIGHT);
+
+		double centerWidth = width - 1.0D / 4.0D * textureWidth;
+		double centerHeight = height - 1.0D / 4.0D * textureHeight;
+
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferBuilder = tessellator.getBuffer();
 		bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-		bufferBuilder.pos(0.0D, 6.0D, 0.0D).tex(6.0D / 16.0D * (xOffset + 0.0D), 6.0D / 16.0D * (yOffset + 1.0D)).endVertex();
-		bufferBuilder.pos(6.0D, 6.0D, 0.0D).tex(6.0D / 16.0D * (xOffset + 1.0D), 6.0D / 16.0D * (yOffset + 1.0D)).endVertex();
-		bufferBuilder.pos(6.0D, 0.0D, 0.0D).tex(6.0D / 16.0D * (xOffset + 1.0D), 6.0D / 16.0D * (yOffset + 0.0D)).endVertex();
-		bufferBuilder.pos(0.0D, 0.0D, 0.0D).tex(6.0D / 16.0D * (xOffset + 0.0D), 6.0D / 16.0D * (yOffset + 0.0D)).endVertex();
+
+		bufferBuilder.pos(0.0D /*         */, 0.0D /*          */, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(0.0D /*         */, 2.0D /*          */, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 0.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D /*         */, 0.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 0.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D + centerWidth, 0.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 2.0D /*          */, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 0.0D /*          */, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 0.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(0.0D /*         */, 2.0D /*          */, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(0.0D /*         */, 2.0D + centerHeight, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D /*         */, 2.0D /*          */, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D /*          */, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 2.0D /*          */, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 1.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(0.0D /*         */, 2.0D + centerHeight, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(0.0D /*         */, 4.0D + centerHeight, 0.0D).tex(0.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 4.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 2.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D /*         */, 2.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D /*         */, 4.0D + centerHeight, 0.0D).tex(1.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 4.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
+		bufferBuilder.pos(2.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(2.0D + centerWidth, 4.0D + centerHeight, 0.0D).tex(2.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 4.0D + centerHeight, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 3.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+		bufferBuilder.pos(4.0D + centerWidth, 2.0D + centerHeight, 0.0D).tex(3.0D / 8.0D + 3.0D / 8.0D * xOffset, 2.0D / 8.0D + 3.0D / 8.0D * yOffset).endVertex();
+
 		tessellator.draw();
 	}
 }
